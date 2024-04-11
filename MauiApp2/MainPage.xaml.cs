7910ae7d06198087e19d08bc63fb00c1519d85f7
Ruben Magaña Alvarado BRX <citi.ruben.magana@EXT-PN4Y2N79FM.local>
2024-04-11 09:28:33 -0600

    initial commit

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MauiApp2.Services;
+//using MetroLog.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	private readonly ILogger _logger;
+
+	
+	public MainPage(ILogger<MainPage> logger)
+	{
+		InitializeComponent();
+		_logger = logger;
+		_logger.LogInformation("\n++++++++++++++ Main Page Constructor");
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+		
+		_logger.LogInformation("\n++++++++++++++ OnCounterClicked MainPage ");
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ MauiApp2/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using MetroLog.MicrosoftExtensions;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp2;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Logging.AddTraceLogger( _ => { });
+		builder.Logging.AddInMemoryLogger( _ => { });
+		builder.Logging.AddStreamingFileLogger( option => 
+		{ 
+			option.RetainDays = 2;               
+            option.FolderPath = Path.Combine(FileSystem.Current.CacheDirectory, "InfoBoardLogs");
+		});
+		
+		builder.Services.AddTransient<MainPage>();
+
+		return builder.Build();
+	}
+}
